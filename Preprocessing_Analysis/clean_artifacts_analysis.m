b7eaefb1bcd5f497775a1412c2f7f04d1800b727
Beesica <103129073+Beesica@users.noreply.github.com>
2024-08-05 18:27:58 +0100

    Update clean_artifacts_analysis.m

--- Preprocessing_Analysis/clean_artifacts_analysis.m
+++ Preprocessing_Analysis/clean_artifacts_analysis.m
@@ -3,42 +3,44 @@ clear all;
 close all;
 clc; 
 
-%% Online code 
+%%  
 addpath('/MATLAB Drive/EEGLAB');
 addpath('/MATLAB Drive/Scripts');
-addpath("EEGLAB/functions/firfilt-master/firfilt-master/");
-addpath("EEGLAB/functions/zapline-plus-main/zapline-plus-main/")
 addpath("EEGLAB/functions/clean_rawdata/")
-addpath("EEGLAB/plugins/amica/")
-addpath("EEGLAB/plugins/ICLabel1.6/")
-addpath("EEGLAB/plugins/preprocessing_helpers/")
 
-eeglab;
+eeglab; % load EEGLAB
 
-savedata = '/MATLAB Drive';
+data = '/MATLAB Drive/data';
+save = '/MATLAB Drive/Images';
 uidname = '02c5e2dc-2cd8-4d48-9d4e-16d55a8fe6d2'; % subject ID
+%%
+% use these to modify clean_artifacts parameter 
+burst = 20; 
+rejection = 'on'; 
+highpass = 'off'; 
 
 %% load data file
-EEG = pop_loadset(sprintf('1a_triggersFiltering_%s.set',uidname),fullfile(savedata)); % data is filtered with 0.1 Hz highpass filter, 128 Hz lowpass filter, & zapline filter (50 Hz, 90 Hz)
+EEG = pop_loadset(sprintf('1a_triggersFiltering_%s.set',uidname),fullfile(data)); % data is filtered with 0.1 Hz highpass filter, 128 Hz lowpass filter, & zapline filter (50 Hz, 90 Hz)
 
 %% Data Cleaning
 clean = pop_reref(EEG, []); % ensure average reference
 
-% clean data
-clean = clean_artifacts(clean, 'Highpass', [0.25 0.75], 'BurstCriterion', 20, 'BurstRejection', 'on'); % adjust parameters to investigate influence of function
+% data cleaning
+clean = clean_artifacts(clean, 'BurstCriterion', burst, 'BurstRejection', rejection, 'Highpass', highpass); 
 
 % interpolate missing channels
 clean = pop_reref(clean, [], 'interpchan', []);
 
-% contrast removed/changed data with raw data
+% visualization of cleaned data
 vis_artifacts(clean, EEG);
 
-% save first segment 
-saveas(gca, sprintf('segment_clean_artifacts_BurstCri_20_BurstRej_on_Highpass_05Hz_%s.jpg', uidname))
+% save first segment
+cd(save)
+saveas(gca, sprintf('segment_clean_artifacts_BurstCri_%d_BurstRej_%s_Highpass_%s_%s.jpg', burst, rejection, num2str(highpass), uidname))
 
 %% face stimuli
 % epoch face stimuli
-EEG_face = pop_epoch(EEG, {'face'}, [-0.5 1.5]);
+EEG_face = pop_epoch(clean, {'face'}, [-0.5 1.5]);
 EEG_face = eeg_checkset(EEG_face); % ensure that dataset is intact
 forLegend_face = 'face'; % save condition name for plot legend
 
@@ -56,7 +58,7 @@ po8_mean_face = EEG_face.mean(po8_idx, :);
 
 %% object stimuli
 % epoch object stimuli
-EEG_object = pop_epoch(EEG, {'object'}, [-0.5 1.5]);
+EEG_object = pop_epoch(clean, {'object'}, [-0.5 1.5]);
 EEG_object = eeg_checkset(EEG_object); % ensure that dataset is intact
 forLegend_object = 'object'; % save condition name for plot legend
 
@@ -74,7 +76,7 @@ po8_mean_object = EEG_object.mean(po8_idx, :);
 
 %% Body Stimuli
 % epoch body stimuli
-EEG_body = pop_epoch(EEG, {'body'}, [-0.5 1.5]);
+EEG_body = pop_epoch(clean, {'body'}, [-0.5 1.5]);
 EEG_body = eeg_checkset(EEG_body); % ensure dataset is intact
 forLegend_body = 'body'; % save condition name for the plot legend
 
@@ -91,14 +93,15 @@ po8_idx = find(strcmp({EEG_body.chanlocs.labels}, 'PO8') == 1);
 po8_mean_body = EEG_body.mean(po8_idx, :);
 
 %% plot ERP for all stimuli
+figure;
+
 % plot face ERP
 plot(EEG_face.times, po8_mean_face, 'DisplayName', forLegend_face)
 
 % set plot specifics
-title(sprintf('Mean Activation at %s of participant %s', 'Po8', uidname), 'FontSize', 11);
 xlabel('time [ms]');
 ylabel('µV');
-xticks([-500, -250, 0, 100, 170, 250, 500, 750, 1000, 1500]);
+xticks([-500, -250, 0, 100, 250, 500, 750, 1000, 1500]);
 xline(0, 'HandleVisibility','off')
 yline(0, 'HandleVisibility','off')
 
@@ -115,6 +118,5 @@ hold off
 legend; % show legend
 
 %% save ERP 
-saveas(gca, sprintf('erp_clean_artifacts_BurstCri_20_BurstRej_on_Highpass_05Hz_%s.jpg', uidname))
-
-
+cd(save)
+saveas(gca, sprintf('erp_clean_artifacts_BurstCri_%d_BurstRej_%s_Highpass_%s_%s.jpg', burst, rejection, num2str(highpass), uidname))
